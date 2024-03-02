03cdae9c39ea1eb4274454640d2ecd98395c956f
Duyet Le <me@duyet.net>
2024-03-02 17:49:06 +0700

    chore(airflow): update airflow

--- airflow/30-dags/sync.sh
+++ airflow/30-dags/sync.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+set -x
+NAMESPACE=${NAMESPACE:-airflow}
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $DIR/../namespace.sh
+source $DIR/../../_helpers.sh
 
 # Change working directory to the directory of this script
 set_working_dir $0
 
-POD=$(kubectl get pods -n $NAMESPACE | grep airflow-scheduler | awk '{print $1}')
+POD=$($kubectl get pods -n $NAMESPACE | grep airflow-scheduler | awk '{print $1}')
 NUM_FILES=$(ls -1 *.py 2>/dev/null | wc -l)
 echo Copying $NUM_FILES dag\(s\) to $POD:/opt/airflow/dags
 
--- airflow/apply.sh
+++ airflow/apply.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 NAMESPACE=airflow
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_helpers.sh
 
 create_namespace $NAMESPACE
 set_current_namespace $NAMESPACE
+
+# with args
+kustomize_apply $DIR $@
